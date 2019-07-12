41de6cf8c544feca2be92302e650a00715714769
Todd Wells <todd.wells@stelligent.com>
2019-07-12 13:24:32 -0700

    add back debug

--- scripts/check_pr_name.rb
+++ scripts/check_pr_name.rb
@@ -3,6 +3,10 @@
 require 'json'
 require 'net/http'
 
+# REMOVE AFTER DONE TESTING
+puts "TOKEN #{ENV['GITHUB_API_TOKEN']}"
+puts "PR #{ENV['CIRCLE_PULL_REQUEST']}"
+
 # ENV['CIRCLE_PULL_REQUEST'] = 'https://github.com/stelligent/cfn_nag/pull/243'
 GITHUB_API_HOST = 'api.github.com'
 REQUEST_PATH = "/repos/#{ENV['CIRCLE_PROJECT_USERNAME']}"\
